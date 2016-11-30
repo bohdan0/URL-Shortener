0d2da1ac8c32c19144487b66c3eced6e82a5d59a
Bohdan Nakonechnyi <bnakonechnuy0@gmail.com>
2016-11-30 15:08:13 -0800

    add visit table and associations andvisit methods

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -6,6 +6,16 @@ class ShortenedUrl < ActiveRecord::Base
     foreign_key: :user_id,
     class_name: :User
 
+  has_many :visits,
+    primary_key: :id,
+    foreign_key: :shortened_url_id,
+    class_name: :Visit
+
+  has_many :visitors,
+    -> { distinct },
+    through: :visits,
+    source: :visitor
+
   def self.random_code
     random_code = SecureRandom.urlsafe_base64
     while ShortenedUrl.exists?(short_url: random_code)
@@ -16,5 +26,17 @@ class ShortenedUrl < ActiveRecord::Base
 
   def self.create_for_user_and_long_url!(user, long_url)
     ShortenedUrl.create!(user_id: user.id, long_url: long_url, short_url: self.random_code)
+  end
+
+  def num_clicks
+    visits.count
+  end
+
+  def num_uniques
+    visitors.count
+  end
+
+  def num_recent_uniques
+    visits.where("created_at > ?", 10.minutes.ago).select(:user_id).distinct.count
   end
 end
